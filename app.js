9c89c78a46cf8a6feb64addd96c0b3bf0ebe8beb
Alan Nguyen <tuan8101@gmail.com>
2020-02-21 00:43:01 -0700

    Create calculateResults function'

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 document
   .getElementById("loan-form")
   .addEventListener("submit", calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log("Calculating...");
+  // UI Vars
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+}
